e5ade087aeb6fefae8ec19a4570e050f06723142
Alex Bodrov <alexvbodrov@mail.ru>
2022-12-03 23:24:24 +0300

    update launcher

--- launcher.py
+++ launcher.py
@@ -14,8 +14,19 @@ while True:
         break
     elif action == 's':
         process.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
-        for num in range(0, int(input('Введите кол-во клиентов'))):
-            process.append(subprocess.Popen(f'python client.py -n client{num}', creationflags=subprocess.CREATE_NEW_CONSOLE))
+        while True:
+            max_clients = 8
+            try:
+                num_clients = int(input(f'Введите кол-во клиентов(от 2 до {max_clients - 1}): '))
+            except:
+                print('ValueError: invalid literal for int() with base 10')
+                continue
+            if isinstance(num_clients, int) and 1 < num_clients < max_clients:
+                for num in range(0, num_clients):
+                        process.append(subprocess.Popen(f'python client.py -n client{num}', creationflags=subprocess.CREATE_NEW_CONSOLE))
+                break
+            else:
+                print(f'Неверный ввод. Введите кол-во клиентов(от 2 до {max_clients - 1})')
     elif action == 'x':
         while process:
             victim = process.pop()
